b41321f3c31950a6e2cd1b46a3461538d7ef5650
giugerlo <ggiuliano526@gmail.com>
2025-01-24 13:36:27 -0300

    creo admin.php

---
+++ admin.php
@@ -0,0 +1,25 @@
+<?php
+//INCLUYO HEAD
+include_once 'includes/head.php';
+?>
+<body class="bg-secondary">
+
+    <?php
+    //INCLUYO HEADER
+    include_once 'includes/header.php';
+    ?>
+    <main>
+        <?php
+        include_once 'templates/admin-main.php';
+        ?>
+    </main>
+    <!-- FOOTER Y SCRIPTS -->
+    <?php
+    include_once 'includes/footer.php';
+    include_once 'includes/scripts.php';
+    ?>
+    
+
+</body>
+
+</html>
